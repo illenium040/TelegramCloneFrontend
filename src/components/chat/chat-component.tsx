b838057f5255172a17c953ef975b21dd84005e1b
illenium040 <illenium040@mail.ru>
2022-09-03 20:04:46 +0300

    useRef is good

--- src/components/chat/chat-component.tsx
+++ src/components/chat/chat-component.tsx
@@ -1,44 +1,43 @@
-import { useCallback, useEffect, useState } from 'react'
-import { IObserver } from '../../extensions/observer-pattern';
-import { MessageDTO } from '../../models/message-models';
-import { ChatListUnit } from '../../models/chat-models';
-import { UserDTO } from '../../models/user-models';
-import { useInjection } from '../../extensions/di-container';
-import { ChatHeader } from './chat-header';
-import { useTypedSelector } from '../../store/store';
-import { useActions } from '../../store/use-actions';
-import { Loading } from '../helpers/loading';
-import { MessageManager } from './messages/message-manager';
+import { createContext, useCallback, useEffect, useState } from 'react'
+import { IObserver } from '../../extensions/observer-pattern'
+import { MessageDTO } from '../../models/message-models'
+import { ChatListUnit } from '../../models/chat-models'
+import { UserDTO } from '../../models/user-models'
+import { useInjection } from '../../extensions/di-container'
+import { ChatHeader } from './chat-header'
+import { useTypedSelector } from '../../store/store'
+import { useActions } from '../../store/use-actions'
+import { Loading } from '../helpers/loading'
+import { MessageManager, MessageReceiver, MessageSender } from './messages/message-manager'
 
 export type ChatProps = {
-    currentUser: UserDTO;
-    targetChat: ChatListUnit;
+    currentUser: UserDTO
+    targetChat: ChatListUnit
 }
-
 
 //idk but have to optimize
 export const ChatComponent = (props: ChatProps) => {
-    const { currentUser, targetChat } = props;
-    const { chatService } = useInjection();
-    const { chat, error, loading } = useTypedSelector(state => state.getChatMessagesReducer);
-    const { getChatMessages } = useActions();
+    const { currentUser, targetChat } = props
+    const { chatService } = useInjection()
+    const { chat, error, loading } = useTypedSelector(state => state.getChatMessagesReducer)
+    const { getChatMessages } = useActions()
 
     //can occur bug with multiple msg
     useEffect(() => {
-        getChatMessages(targetChat.chatId, chatService);
-    }, []);
+        getChatMessages(targetChat.chatId, chatService)
+    }, [])
 
     useEffect(() => {
-        if (loading) return;
-        getChatMessages(targetChat.chatId, chatService);
-    }, [targetChat]);
+        if (loading) return
+        getChatMessages(targetChat.chatId, chatService)
+    }, [targetChat])
 
-    if (error) return (<div>{error}</div>)
+    if (error) return <div>{error}</div>
     if (loading) return <Loading />
     return (
-        <div className='chat'>
+        <div className="chat">
             <ChatHeader userName={targetChat.user.name} />
             <MessageManager chat={chat!} currentUser={currentUser} targetChat={targetChat} />
-        </div >
+        </div>
     )
 }
