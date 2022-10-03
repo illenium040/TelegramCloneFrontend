dd50f4e735c9eae0529beb2391c4e21c8af67f39
illenium040 <illenium040@mail.ru>
2022-10-03 16:07:49 +0300

    fix style path

--- src/pages/Sidebar/Sidebar.tsx
+++ src/pages/Sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import "./sidebar.css"
+import "../Sidebar/sidebar.css"
 import { BsFillChatFill } from "@react-icons/all-files/bs/BsFillChatFill"
 import { BsJustify } from "@react-icons/all-files/bs/BsJustify"
 import { BsFolder } from "@react-icons/all-files/bs/BsFolder"
